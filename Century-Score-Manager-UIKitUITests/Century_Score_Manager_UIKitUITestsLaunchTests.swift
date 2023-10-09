4b0c47dbc3a44b595f86a86111b67622cdf0408e
faizan-tanveer-brainx <faizan.tanveer@brainxtech.com>
2023-10-09 17:11:39 +0500

    Initial Commit

---
+++ Century-Score-Manager-UIKitUITests/Century_Score_Manager_UIKitUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Century_Score_Manager_UIKitUITestsLaunchTests.swift
+//  Century-Score-Manager-UIKitUITests
+//
+//  Created by BrainX Technologies 11 on 09/10/2023.
+//
+
+import XCTest
+
+final class Century_Score_Manager_UIKitUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
